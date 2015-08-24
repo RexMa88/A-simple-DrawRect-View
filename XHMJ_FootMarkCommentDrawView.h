8f24fdaec25fdcd1abb9d74f404d70ff0aa2d3ce
RexMa <rex.smith98@gmail.com>
2015-08-24 14:56:16 +0800

    Create XHMJ_FootMarkCommentDrawView.h

---
+++ XHMJ_FootMarkCommentDrawView.h
@@ -0,0 +1,13 @@
+//
+//  XHMJ_FootMarkCommentDrawView.h
+//  chinapainting
+//
+//  Created by RexMa on 15/8/24.
+//  Copyright (c) 2015年 XinHuaTV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XHMJ_FootMarkCommentDrawView : UIView
+
+@end
